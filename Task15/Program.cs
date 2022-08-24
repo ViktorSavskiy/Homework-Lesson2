efd3a6e76180a0f97ebd2e22444bb9f6c5e366ff
Viktor <savskij@bk.ru>
2022-08-24 22:44:27 +0300

    Task15

---
+++ Task15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите цифру от 1 до 7");
+int num = int.Parse(Console.ReadLine());
+if (num == 6 || num == 7)
+{
+  Console.WriteLine("Выходной");  
+}
+if (num == 1 || num == 2 || num == 3 || num == 4 || num == 5)
+{
+   Console.WriteLine("Не выходной");   
+}
+if (num > 7 || num < 1)
+{
+  Console.WriteLine("Введите цифру от 1 до 7");  
+}
