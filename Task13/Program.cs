765f4aec227682683cb6aaad2c756a299ebd2368
Viktor <savskij@bk.ru>
2022-08-24 23:06:37 +0300

    Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+if (num > 99)
+{
+int a = num/100;
+int b = a%10;
+ Console.WriteLine(b);   
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет"); 
+}
